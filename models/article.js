c1e725d3c80f405f608fe0b3d56cf9ebd8e27a33
yuhan <yh891926@dal.ca>
2021-11-15 00:18:03 -0400

    add slug and sanitizer

--- models/article.js
+++ models/article.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const { marked } = require('marked')
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
 
 const articleSchema = new Schema({
     title: {
@@ -16,7 +21,28 @@ const articleSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    sanitizeHtml: {
+        type: String,
+        required: true
     }
+})
+
+articleSchema.pre('validate', function(next) {
+    // create article slug from its title
+    if (this.title) {
+        this.slug = slugify(this.title, { lower: true, strict: true })
+    }
+    // sanitize HTML
+    if (this.markdown) {
+        this.sanitizeHtml = dompurify.sanitize(marked(this.markdown))
+    }
+    next()
 })
 
 const Article = mongoose.model('Article', articleSchema)
