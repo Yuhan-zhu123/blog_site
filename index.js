e471c601b4d2c7148eaca1e3b8fde0aa7a1461b3
yuhan <yh891926@dal.ca>
2021-11-15 00:25:15 -0400

    update index.js file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const articleRouter = require('./routes/articles')
 const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 const port = 3000
 
@@ -23,24 +24,10 @@ connection.once("open", function() {
 // use EJS as template engine
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))// override the method
 
-app.get('/', (req, res) => {
-    const articles = [{ // some test data
-        title: 'Title 1',
-        createdAt: new Date(),
-        description: 'Test description1'
-    },
-    {
-        title: 'Title 2',
-        createdAt: new Date(),
-        description: 'Test description2'
-    },
-    {
-        title: 'Title 3',
-        createdAt: new Date(),
-        description: 'Test description3'
-    }
-    ]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({createdAt: -1}) //sort articles in descending order by created time
     res.render('articles/index', { articles: articles })
 })
 
